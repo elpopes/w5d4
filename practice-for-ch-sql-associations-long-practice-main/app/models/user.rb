be493b6c6c560c10fbf43d2a34f1502462f14d4b
Jiong Qi <jiongqipan@gmail.com>
2022-12-08 15:31:44 -0500

    first

--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -1,2 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :enrollment_id,
+        class_name: :Enrollment
+
+    # has_many :enrolled_courses,
+    #     primary_key: :id,
+    #     foreign_key: :enrolled_course_id
+
+    has_many :enrolled_courses,
+		through: :enrollements,
+		source: :chirp
 end
+
+has_many :liked_chirps,
+through: :likes,                 # refers to has_many: likes
+source: :chirp
